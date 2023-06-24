2a2b443f9376bfb925d98bb0b70476d2369bcc63
Nitish <79391172+TechSupportz@users.noreply.github.com>
2023-06-24 16:48:34 +0800

    first commit

---
+++ app/src/index.ts
@@ -0,0 +1,21 @@
+import Koa from "koa"
+import bodyParser from "koa-bodyparser"
+import json from "koa-json"
+import { zodRouter } from "koa-zod-router"
+
+const app = new Koa()
+const router = zodRouter()
+
+app.use(bodyParser())
+app.use(json())
+
+router.get("/", async (ctx) => {
+	ctx.status = 200
+	ctx.body = "Transito's server is running as expected!"
+})
+
+app.use(router.routes())
+
+app.listen(8080, () => {
+	console.log(`Server running on http://localhost:8080`)
+})
