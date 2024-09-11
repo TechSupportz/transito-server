5c1dbb273855a9a29e21a3fe526ae2ad084d9771
Nitish <tnitish654@gmail.com>
2024-09-11 22:41:35 +0800

    ⚡ Update server to use https for LTA DataMall

--- src/utils/lta-api.ts
+++ src/utils/lta-api.ts
@@ -1,4 +1,4 @@
-export const ltaBaseUrl = "http://datamall2.mytransport.sg/ltaodataservice"
+export const ltaBaseUrl = "https://datamall2.mytransport.sg/ltaodataservice"
 
 export const ltaAPIHeaders: HeadersInit = {
 	AccountKey: process.env.LTA_API_KEY ?? "",
