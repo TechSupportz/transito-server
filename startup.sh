80506b3a0727e8d8c3f872cd5009854a0e5ce27c
Nitish <tnitish654@gmail.com>
2026-06-21 01:06:00 +0800

    🛠️  Debugging GCE

--- startup.sh
+++ startup.sh
@@ -1,11 +1,21 @@
 #! /bin/bash
 
+set -Eeuo pipefail
+
+export DOCKER_CONFIG="/var/lib/transito-docker-config"
+mkdir -p "$DOCKER_CONFIG"
+
 docker-credential-gcr configure-docker --registries us-west1-docker.pkg.dev
 
 IMAGE_URL="us-west1-docker.pkg.dev/transito-8f50c/transito-server/transito-server:latest"
 
+docker pull "$IMAGE_URL"
+
 docker stop transito-server || true
 docker rm transito-server || true
+
+docker stop cloudflared-tunnel || true
+docker rm cloudflared-tunnel || true
 
 # 4. FETCH SECRETS
 LTA_API_KEY=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/LTA_API_KEY" -H "Metadata-Flavor: Google")
@@ -17,6 +27,12 @@ ONEMAP_EMAIL=$(curl -s "http://metadata.google.internal/computeMetadata/v1/insta
 ONEMAP_PASSWORD=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/ONEMAP_PASSWORD" -H "Metadata-Flavor: Google")
 
 CF_TOKEN=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/CF_TOKEN" -H "Metadata-Flavor: Google")
+
+UNIVUS_APP_API=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/UNIVUS_APP_API" -H "Metadata-Flavor: Google")
+
+UNIVUS_HTD_API=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/UNIVUS_HTD_API" -H "Metadata-Flavor: Google")
+
+NUS_ETA_TOKEN=$(curl -s "http://metadata.google.internal/computeMetadata/v1/instance/attributes/NUS_ETA_TOKEN" -H "Metadata-Flavor: Google")
 
 # 5. START CF TUNNEL
 docker run -d \
@@ -34,6 +50,9 @@ docker run -d \
   -e SECRET="$JSON_SECRET" \
   -e ONEMAP_EMAIL="$ONEMAP_EMAIL" \
   -e ONEMAP_PASSWORD="$ONEMAP_PASSWORD" \
+  -e UNIVUS_APP_API="$UNIVUS_APP_API" \
+  -e UNIVUS_HTD_API="$UNIVUS_HTD_API" \
+  -e NUS_ETA_TOKEN="$NUS_ETA_TOKEN" \
   -e PORT=80 \
   $IMAGE_URL
 
